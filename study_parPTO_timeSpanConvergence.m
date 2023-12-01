d88fe8416a9efe8664e78fd23b6a1c4302008cf5
Jeremy Simmons <simmo536@umn.edu>
2023-12-01 12:19:03 -0600

    add study for convergence of metric wrt length of simulation

---
+++ study_parPTO_timeSpanConvergence.m
@@ -0,0 +1,203 @@
+% study_parPTO_timeSpanConvergence.m script m-file
+% AUTHORS:
+% Jeremy Simmons (email: simmo536@umn.edu)
+% University of Minnesota
+% Department of Mechanical Engineering
+%
+% CREATION DATE:
+% 12/01/2023
+%
+% PURPOSE/DESCRIPTION:
+% This script performs parameter variation studies
+% using the model contained in sys_parPTO.m and solved by
+% sim_parPTO.m.
+% The parameter initiallization functions are called within this
+% script before the sim_parPTO.m script is called.
+%
+% This specific script studies the length of the simulation.
+%
+% This script is set up to be run as part of a SLURM job array. The
+% following lines are required before this script is called:
+%   iVar = ${SLURM_ARRAY_TASK_ID};
+%   SS=1;
+%
+% FILE DEPENDENCY:
+% ./Parallel-type PTO/
+%   initialConditionDefault_parPTO
+%   parameters_parPTO.m
+%   sim_parPTO.m
+%   stateIndex_parPTO.m
+%   sys_parPTO.m
+% ./WEC model/
+%   flapModel.m
+%   hydroStaticTorque.m
+%   parameters_WECmodel.m
+% ./WEC model/WECdata
+%   nemohResults_vantHoff2009_20180802.mat
+%   vantHoffTFCoeff.mat
+% ./Solvers/
+%   deltaE_NI.m
+%   deltaV_NI.m
+%   ode1.m
+% ./Components/
+%   areaFracPWM.m
+%   capAccum.m
+%   deadVCap.m
+%   flowCV.m
+%   flowPRV.m
+% ./Components/Pipeline
+%   flowR.m
+%   lineCap.m
+%   pipelineNPi.m
+% ./Utilities/
+%   get_current_git_hash.m
+%   leadingZeros.m
+%
+% UPDATES:
+% 12/01/2023 - Created from study_parPTO_timeStepConvergence.m.
+%
+% Copyright (C) 2023  Jeremy W. Simmons II
+% 
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+% 
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+% 
+%   You should have received a copy of the GNU General Public License
+%   along with this program. If not, see <https://www.gnu.org/licenses/>.
+%
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clear
+% clc
+addpath('WEC model') 
+addpath(['WEC model' filesep 'WECdata']) 
+addpath('Parallel-type PTO')
+addpath('Components')
+addpath(['Components' filesep 'Pipeline'])
+addpath('Sea States')
+addpath('Solvers')
+addpath('Utilities')
+git_hash_string = get_current_git_hash();
+
+%% %%%%%%%%%%%%   Study Variables  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+nVar = 50;
+tspan = linspace(100,3000,nVar);% [m^3/s/Pa] valve coefficient for high-pressure outlet check valve
+
+saveSimData = 1; % save simulation data (1) or just output variables (0)
+
+%% %%%%%%%%%%%%   SIMULATION PARAMETERS  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Simulation timeframe
+par.Tramp = 250; % [s] excitation force ramp period
+par.tstart = 0; %[s] start time of simulation
+par.tend = tspan(iVar) + par.tstart; %[s] end time of simulation
+
+% Solver parameters
+% par.odeSolverRelTol = 1e-4; % Rel. error tolerance parameter for ODE solver
+% par.odeSolverAbsTol = 1e-4; % Abs. error tolerance parameter for ODE solver
+par.MaxStep = 5e-5;             % [s] for fixed time solver, this is the step size for solver
+par.downSampledStepSize = 1e-2; % [s] specifies time step for data output
+if mod(par.downSampledStepSize,par.MaxStep)
+    warning('down-sampled time step is not an integer multiple of the maximum step size')
+end
+
+% Sea State and Wave construction parameters
+Hs = [2.34 2.64 5.36 2.05 5.84 3.25];
+Tp = [7.31 9.86 11.52 12.71 15.23 16.5];
+par.wave.Hs = Hs(SS);
+par.wave.Tp = Tp(SS);
+par.WEC.nw = 1000; % num. of frequency components for harmonic superposition
+par.wave.rngSeedPhase = 3; % seed for the random number generator
+
+% load parameters
+par = parameters_parPTO(par,...
+    'nemohResults_vantHoff2009_20180802.mat','vantHoffTFCoeff.mat');
+
+% Define initial conditions
+stateIndex_parPTO % load state indices, provides 'iy_...'
+initialConditionDefault_parPTO % default ICs, provides 'y0'
+
+%% Special modifications to base parameters
+% par.Sro = 3700; % [m^3]
+% par.D_WEC = 0.23;         % [m^3/rad] flap pump displacement
+p_ro_nom = 1e6*[4.0000 4.9435 8.0000 5.2661 8.0000 7.1052]; % [Pa]
+par.control.p_ro_nom = p_ro_nom(SS); % [Pa]
+
+par.ERUconfig.present = 1;
+par.ERUconfig.outlet = 1;
+
+par.rvConfig.included = 0; % RO inlet valve is 1 - present, 0 - absent
+par.rvConfig.active = (0)*par.rvConfig.included; % RO inlet valve is 1 - active, 0 - passive
+
+par.w_c = (3500)*2*pi/60; % [(rpm) -> rad/s]
+
+%% %%%%%%%%%%%%   COLLECT DATA  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% run simulation
+ticSIM = tic;
+out = sim_parPTO(y0,par);
+toc(ticSIM)
+
+% Calculate metrics
+Ebal_error = out.Ebal_error;
+Vbal_error = out.Vbal_error;
+
+% for electrical power balance
+deltaE_battery = out.power.deltaE_battery;
+
+% for LPaccum
+p_loutMean = mean(out.p_lout);
+ % Variation in pressure at WEC-driven pump inlet
+p_loutMax = max(out.p_lout);
+p_loutMin = min(out.p_lout);
+p_loutVar = var(out.p_lout);
+p_loutStd = std(out.p_lout);
+ % Minimum pressure in WEC-driven pump chambers
+p_wpMin = min(min(out.p_a),min(out.p_b));
+
+ % Electric power consumption of charge pump
+P_cElec = mean(out.power.P_cElec);
+P_cElec_norm = P_cElec/mean(out.power.P_WEC);
+ % Power losses from charge pump
+P_cLoss = mean(out.power.P_cLoss);
+L_c = P_cLoss/mean(out.power.P_WEC);
+
+ % power loss from pipeline
+P_LPPL = out.power.P_LPPL;
+L_LPPL = P_LPPL/mean(out.power.P_WEC);
+
+% for accum_woRV and accum_wRV
+q_permMean = mean(out.q_perm);
+PP_WEC = mean(out.power.P_WEC);
+PP_wp = mean(out.power.P_wp);
+PP_rv = mean(out.power.P_rv);
+PP_hinPRV = mean(out.power.P_hinPRV);
+PP_roPRV = mean(out.power.P_roPRV);
+dpdt_max = max(abs(out.dydt(:,iyp_ro)));
+
+dist_dpdt = statsTimeVar_cdf(out.t,abs(out.dydt(:,iyp_ro)));
+dpdt_97 = dist_dpdt.xi(find(dist_dpdt.f > 0.97,1,'first'));
+
+if ~saveSimData
+     clearvars out
+end
+
+%% %%%%%%%%%%%%   Save Data  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Save: time in ISO8601
+filename = ['data_parPTO_timeSpanConvergence', ...
+            '_',char(datetime("now",'Format','yyyyMMdd')), ...
+            '_',num2str(SS,leadingZeros(999)), ...
+            '_',num2str(iVar,leadingZeros(nVar))];
+save(filename,'-v7.3')
+
+%% %%%%%%%%%%%%   End Computations  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+poolobj = gcp('nocreate'); delete(poolobj);
+
+return
