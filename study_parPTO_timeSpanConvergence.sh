76c51aef07732bf33dde014901fb91df1ae56a16
Jeremy Simmons <simmo536@umn.edu>
2023-12-01 12:34:44 -0600

    add slurm script for time span convergence study

---
+++ study_parPTO_timeSpanConvergence.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10gb
+#SBATCH -t 24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=simmo536@umn.edu
+#SBATCH -p small
+#SBATCH -o %A_%a.out
+#SBATCH -e %A_%a.err
+
+cd ~/2023-DynPTOModelDesignStudies
+module load matlab
+matlab -nodisplay -r \
+"iVar = ${SLURM_ARRAY_TASK_ID}; \
+display(['iVar = ',num2str(iVar)]); \
+SS = ${SS}; \
+display(['SS = ',num2str(SS)]); \
+study_parPTO_timeSpanConvergence"
+
+# Commands to use
+# sbatch --export=SS=2 --array=1-50 ~/2023-DynPTOModelDesignStudies/study_parPTO_timeSpanConvergence.sh
+# dos2unix  study_parPTO_timeStepConvergence.sh
+
