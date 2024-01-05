7d152c529053d867c973a6be4306342d8904a454
Jeremy Simmons <simmo536@umn.edu>
2024-01-05 15:26:22 -0600

    adjusted grid for study variables for another preliminary run

--- study_parPTO_LPaccum.m
+++ study_parPTO_LPaccum.m
@@ -141,18 +141,18 @@ par.pc_lout = 0.15e6; % [Pa] charge pressure
 %% %%%%%%%%%%%%   Study Variables  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % total low-pressure accumulator volume
 nVar1 = 10;
-Vc_l = 1e-3*logspace(log10(100),log10(3000),nVar1); % [(L) -> m^3]
+Vc_l = 1e-3*logspace(log10(500),log10(10000),nVar1); % [(L) -> m^3]
 
 % portion of low-pressure accumulator volume at WEC-driven pump inlet
-X = 1 - logspace(log10(1-0.3),log10(1-0.97),5);
+X = 1 - logspace(log10(1-0.5),log10(1-0.99),5);
 nVar2 = numel(X);
 
 % Diameter of low-pressure pipeline
 nVar3 = 10;
-d_LPPL = linspace(0.05,0.18,nVar3); % [m]
+d_LPPL = linspace(0.05,0.2,nVar3); % [m]
 
 % charge pump speed
-w_c = (1700:100:3500)*2*pi/60; % [(rpm) -> rad/s]
+w_c = (1600:100:3500)*2*pi/60; % [(rpm) -> rad/s]
 nVar4 = numel(w_c);
 
 [meshVar3D.Vc_l, meshVar3D.X, meshVar3D.d_LPPL] = meshgrid(Vc_l,X,d_LPPL);
