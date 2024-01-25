43a9a8124b42aa2267bc837d46a3ac4ad8cbf250
Jeremy Simmons <simmo536@umn.edu>
2024-01-25 13:02:32 -0600

    add plot of power losses

--- Post-processing scripts/postprocess_parPTO_accum_woPL_woRV.m
+++ Post-processing scripts/postprocess_parPTO_accum_woPL_woRV.m
@@ -1,4 +1,4 @@
-X = 1e3*Vc_h;
+X = Vc_h;
 par = parBase;
 % select variable to plot
 maxOr97 = 1;
@@ -24,7 +24,7 @@ linestyles = {'-', '--', ':', '-.'};
 bottomEdge = 1;
 leftEdge = 3;
 width = 7.5; % one column: 3+9/16, two column: 7.5
-height = 3.25;
+height = 5;
 fontSize = 9;
 lineWidth = 1;
 
@@ -34,21 +34,25 @@ fig = figure;
 fig.Units = 'inches';
 fig.Position = [leftEdge bottomEdge width height ];
 
-n_plots = 1;
-ax1 = subplot(n_plots,1,1);
-ax1.FontName = 'times';
-ax1.FontSize = fontSize-1;
+n_plots = 2;
+
+iax = 1;
+ax(iax) = subplot(n_plots,1,1);
+ax(iax).FontName = 'times';
+ax(iax).FontSize = fontSize-1;
+
+
+
+p(iax,1) = semilogy(X,Y,'-k', ...
+    'LineWidth',1);
 
 hold on
 
-plot(X([1 end]),1e-3*par.control.dpdt_ROmax*[1 1],'--k')
+p(iax,2) = plot(X([1 end]),1e-3*par.control.dpdt_ROmax*[1 1],'--k')
 legLabels(1) = convertCharsToStrings( ...
         ['target limit']);
 
-p = plot(X,Y,'-k', ...
-    'LineWidth',1);
-
-xlabel('volume (L)', ...
+xlabel('volume (1000L)', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
 ylabel('rate of change in pressure (kPa/s)', ...
 'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
@@ -66,3 +70,46 @@ set(leg, 'Location', 'best')
 yLim = ylim;
 ylim([0 yLim(2)])
 % ylim([0 1e-3*1.5*par.control.dpdt_ROmax])
+
+% %%%%%%%%%%%% power loss
+Y = 100*(PP_hinPRV + PP_roPRV + PP_genLoss +  PP_pmLoss)./PP_WEC;
+iax = 2;
+ax(iax) = subplot(n_plots,1,iax);
+ax(iax).FontName = 'times';
+ax(iax).FontSize = fontSize-1;
+
+
+p(iax,1) = semilogy(X,Y,'-k', ...
+    'LineWidth',1);
+hold on
+p(iax,2) = semilogy(X,100*PP_hinPRV./PP_WEC,'-r', ...
+    'LineWidth',1);
+p(iax,3) = semilogy(X,100*PP_roPRV./PP_WEC,'-g', ...
+    'LineWidth',1);
+p(iax,4) = semilogy(X,100*(PP_genLoss +  PP_pmLoss)./PP_WEC,'-b', ...
+    'LineWidth',1);
+
+legLabels(1) = convertCharsToStrings( ...
+        ['combined','WEC outlet PRV','RO feed PRV','pump/motor and generator']);
+
+
+xlabel('volume (1000L)', ...
+'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
+ylabel('power loss ($\%$)', ...
+'Interpreter','latex','FontSize',fontSize-1,'fontname','Times')
+title(['Portion of WEC Power lost to PRVs: Sea State ',num2str(SS)],...
+'Interpreter','latex','FontSize',fontSize,'fontname','Times')
+
+leg = legend(legLabels);
+leg.FontSize = fontSize-1;
+leg.FontName = 'Times';
+% rect = [0.5, -0.2, 0.25, 0.15];
+% set(leg, 'Position', rect)
+set(leg, 'Location', 'best')
+% set(leg, 'Location', 'southoutside')
+% xlim([0 max(Vtotal)])
+yLim = ylim;
+ylim([0 yLim(2)])
+% ylim([0 1e-3*1.5*par.control.dpdt_ROmax])
+
+linkaxes(ax,'x')
